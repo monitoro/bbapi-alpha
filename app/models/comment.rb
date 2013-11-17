06cbfbda769d4237da31476d41fbb12328b45ccf
Kim Daekwon <propellerheaven@gmail.com>
2013-11-17 16:41:49 +0900

    comment 와 bookeepings에 activities 추가

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 class Comment < ActiveRecord::Base
+  include PublicActivity::Model
+  
   belongs_to :writer, class_name: 'User'
   belongs_to :commentable, polymorphic: true
 
   # 유효성 검증 => writer_id, commentable_id 는 필수항목임
   validates_presence_of :writer_id , :commentable_id
+
+  tracked owner: ->(controller, model) { controller && controller.current_user }
+  tracked recipient: ->(controller, model) { model.writer }
 end
